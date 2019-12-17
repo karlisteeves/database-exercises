961a93b9ef6998d91624246802ba185a073ec167
Karli Steeves <steeves.karli@gmail.com>
2019-12-17 15:43:14 -0600

    working on mysql

---
+++ student_migration.sql
@@ -0,0 +1,11 @@
+USE deimos_db;
+CREATE TABLE IF NOT EXISTS
+students(
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL,
+birthday DATE,
+projects TEXT NOT NULL,
+is_hired TINYINT NULL,
+PRIMARY KEY(id)
+);
