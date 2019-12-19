4ee61987ed633f8e72c06bbc2406c93957900870
Karli Steeves <steeves.karli@gmail.com>
2019-12-19 10:18:34 -0600

    working through exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,47 +2,28 @@ use employees;
 
 select *
 from employees
-where first_name in ('Irena', 'Vidya', 'Maya');
+where first_name in ('Irena', 'Vidya', 'Maya')
+order by last_name, first_name desc;
 
 select *
 from employees
-where last_name like 'E%';
-
-select *
-from employees
-where hire_date between '1990-01-01' and '2000-01-01';
-
-select *
-from employees
-where birth_date like '%%%%-12-25';
-
-select *
-from employees
-where last_name like '%q%';
-
-select *
-from employees
-where gender = 'M'
-  and (first_name = 'Irena'
-  or first_name = 'Vidya'
-  or first_name = 'Maya');
+where last_name like 'E%'
+order by emp_no desc;
 
 select *
 from employees
 where last_name like '%e'
-   or last_name like 'e%';
+   or last_name like 'e%'
+order by emp_no desc;
 
 select *
 from employees
 where last_name like '%e'
-  and last_name like 'e%';
+  and last_name like 'e%'
+order by emp_no desc;
 
 select *
 from employees
 where hire_date between '1990-01-01' and '2000-01-01'
-  and birth_date like '%%%%-12-25';
-
-select *
-from employees
-where last_name like '%q%'
-  and not last_name like '%qu%';
+  and birth_date like '%%%%-12-25'
+order by birth_date, hire_date desc;
